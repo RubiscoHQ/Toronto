d37429b4a9773e50118b7077edde84762eb4f4a2
Hanqing Liu <3120101774@zju.edu.cn>
2016-08-18 00:42:58 -0400

    start

---
+++ README.md
@@ -0,0 +1,104 @@
+## Input Data
+
+### Reference Genome
+- Hg19
+
+### ExAC data source
+- ExAC release 0.3.1
+	> We have removed individuals affected by severe pediatric disease, so this data set should serve as a useful reference set of allele frequencies for severe disease studies.[[http://exac.broadinstitute.org/about](http://exac.broadinstitute.org/about)]  
+	> [ftp://ftp.broadinstitute.org/pub/ExAC\_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz](ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz)
+	> use annovar convert2annovar.pl to convert annovar input  
+	> `> perl convert2annovar.pl -format vcf4 ExAC.r0.3.1.sites.vep.vcf > ExAC.r0.3.1.sites.vep.vcf.avinput  
+	> > NOTICE: Finished reading 9362538 lines from VCF file  
+	> > NOTICE: A total of 9362318 locus in VCF file passed QC threshold, representing 9415611 SNPs (6265499 transitions and 3150112 transversions) and 780261 indels/substitutions  
+	> > NOTICE: Finished writing 9415611 SNP genotypes (6265499 transitions and 3150112 transversions) and 780261 indels`
+- ExAC on non-TCGA samples based on release 0.3  
+	[http://annovar.openbioinformatics.org/en/latest/user-guide/download/](http://annovar.openbioinformatics.org/en/latest/user-guide/download/)
+- ExAC on non-Psychiatric disease samples based on release 0.3  
+	[http://annovar.openbioinformatics.org/en/latest/user-guide/download/](http://annovar.openbioinformatics.org/en/latest/user-guide/download/)
+- ExAC non-both  
+	Merge both non-psy and non-tcga. 
+
+### Clinvar data source
+- Clinvar\_20160302  
+	[http://annovar.openbioinformatics.org/en/latest/user-guide/download/](http://annovar.openbioinformatics.org/en/latest/user-guide/download/)
+
+### Annotation data source
+- refGene (gene based annotation)  
+	[http://annovar.openbioinformatics.org/en/latest/user-guide/download/](http://annovar.openbioinformatics.org/en/latest/user-guide/download/)
+- dbNSFP version 3.0a   
+	(whole-exome SIFT, PolyPhen2 HDIV, PolyPhen2 HVAR, LRT, MutationTaster, MutationAssessor, FATHMM, MetaSVM, MetaLR, VEST, CADD, GERP++, DANN, fitCons, PhyloP and SiPhy scores from )  
+	[https://sites.google.com/site/jpopgen/dbNSFP](https://sites.google.com/site/jpopgen/dbNSFP)  
+	[http://annovar.openbioinformatics.org/en/latest/user-guide/download/](http://annovar.openbioinformatics.org/en/latest/user-guide/download/)
+
+### Annotation software
+- ANNOVAR  
+	All annotation database also download form Annovar  
+	[http://annovar.openbioinformatics.org/en/latest/](http://annovar.openbioinformatics.org/en/latest/)  
+
+### Data processing
+1. Using annovar to annotate respective input databases with refGene, dbNSFP, Clinvar  
+	`perl table_annovar.pl inputfile humandb/ -buildver hg19 -out exportfile -protocol refGene,dbnsfp30a,clinvar_20160302 -operation g,f,f -nastring NA -remove --thread 4 --maxgenethread --onetranscript`
+2. Filtered out mutations  
+	- by type, remain only non-syndromic mutations  
+	- by Pathogenicity: for ExAC mutations exclude those annotated “Pathogenic” by Clinvar; for Clinvar only remains “Pathogenic” mutations
+3. Annotate ExAC vcf information using ExAC.r0.3.1.sites.vep.vcf
+4. ExAC HighQuality Filter  
+	Generate HQ group based on ExAC high quality definition
+	> (1) they were given a PASS filter status by VQSR,   
+	> (2) at least 80% of the individuals in the dataset had at least depth (DP) \>= 10 and genotype quality (GQ) \>= 20 (i.e. AN_Adj \>= 60706*0.8*2 or 97130),   
+	> (3) there was at least one individual harboring the alternate allele with depth \>= 10 and GQ \>= 20,  
+	> (4) the variant was not located in the 10 1-kb regions of the genome with the highest levels of multi-allelic (quad-allelic or higher) variation.  
+	> ##    chrom bin_start   bin_end freq  
+	> ## 1     14 106330000 106331000  109  
+	> ## 2      2  89160000  89161000  103  
+	> ## 3     14 106329000 106330000   76  
+	> ## 4     14 107178000 107179000   63  
+	> ## 5     17  18967000  18968000   46  
+	> ## 6     22  23223000  23224000   44  
+	> ## 7      1 152975000 152976000   42  
+	> ## 8      2  89161000  89162000   39  
+	> ## 9     14 107179000 107180000   38  
+	> ## 10    17  19091000  19092000   38
+5. ExAC at least one Hom mutation  
+	Generate 1hom group based on AC\_Hom \>= 1, which means at least one individual carry a homozygous allele.
+6. retrench and generate input file  
+	extract useful informations for analysis. Score values, MAF (using ExAC popmax), Gene and so on.  
+	Defining 6 Freq group according to MAF:  
+	I:   singleton, AN\_All == 1;  
+	II:  2-10 alleles, 2 \<= AN\_All \<= 10;  
+	III: 0.0001 \<= AF\_All \<= 0.001  
+	IV:  0.001 \<= AF\_All \<= 0.01  
+	V:   0.01 \<= AF\_All \<= 0.1  
+	VI:  0.1 \<= AF\_All  
+	 
+7. remove all variants contain NA value for all analyzed scores (SIFT, Polyphen2\_HDIV, Polyphen2\_HVAR, MutationTaster, MutationAssessor, PROVEAN, VEST3, CADD, DANN, fathmm-MKL\_coding, MetaSVM, MetaLR, GERP++\_RS, phyloP7way\_vertebrate, phyloP20way\_mammalian, phastCons7way\_vertebrate, phastCons20way\_mammalian, SiPhy\_29way\_logOdds, 18 scores. Discard LRT and FATHMM for too many NAs).
+
+### Input File
+1. ExAC.all.hg19\_multianno.new.vcf.hq.1hom.input.narm.txt
+2. ExAC.all.hg19\_multianno.new.vcf.hq.input.narm.txt
+3. ExAC.nonpat.hg19\_multianno.new.vcf.hq.1hom.input.narm.txt
+4. ExAC.nonpat.hg19\_multianno.new.vcf.hq.input.narm.txt
+5. clinvar.hg19\_multianno.filt.new.narm.txt
+
+Currently, use 3 as control dataset and 5 as pathogenic dataset
+
+## Data Analysis
+1. Read input file as Patho and Control
+2. Analysis data in different levels  
+	Score [score\_list] ×   
+	Group [group list] ×   
+	select [genome, gene\_specific\_select (gss)] ×   
+	contaminate [True, False] ×   
+	contaminate\_rate [contaminate\_start to contaminate\_end, step = contaminate\_step]  
+	  
+	- Group is the frequent group defined in data processing;  
+	- Select: genome is genome-wide randomly selecting control data; gss is selecting same number of control mutation for each gene according to the number in path dataset.  
+	- contaminate is always False for gss selection  
+	- contaminate preform like: if contaminate\_rate = 0.05 and contaminate\_select = 9000, then randomly select 9000 patho mutations as patho, (9000×0.05 patho + 9000×0.95 ctrl) as ctrl.  
+	  
+	For each analysis level, calculate their PRC, AUPRC and other data.
+3. Write all AUPRC, PR90, PR10 data into out\_file
+4. Draw plots
+5. Analysis out\_file  
+
---
+++ main.py
@@ -0,0 +1,429 @@
+from random import sample
+import pandas as pd
+from pandas import DataFrame
+from numpy import array
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+
+# File routes
+import_dir = '/Users/rubisco/Desktop/Toronto/Mutation_analyse/input/'
+output_dir = '/Users/rubisco/Desktop/Toronto/Mutation_analyse/output/'
+patho_file = 'clinvar.hg19_multianno.filt.new.narm.txt'
+ctrl_file = 'ExAC.all.hg19_multianno.new.vcf.hq.input.narm.txt'
+out_file_name = 'analysis.log.txt'
+
+# Score to analyze
+score_list = ['SIFT']
+group_select_list = ['I', 'II', 'III', 'IV', 'V', 'VI']
+
+# Contaminate setting
+contaminate_select = 9000
+contaminate_start = 0.00
+contaminate_end = 0.40
+contaminate_step = 0.01
+
+
+out_file = open(output_dir + out_file_name, 'w')
+column_name = ['Chr', 'Pos', 'Ref', 'Alt', 'Gene_refGene',
+               'SIFT', 'Polyphen2_HDIV', 'Polyphen2_HVAR', 'MutationTaster', 'MutationAssessor',
+               'PROVEAN', 'VEST3', 'CADD', 'DANN', 'fathmm-MKL_coding', 'MetaSVM', 'MetaLR',
+               'GERP++_RS', 'phyloP7way_vertebrate', 'phyloP20way_mammalian',
+               'phastCons7way_vertebrate', 'phastCons20way_mammalian', 'SiPhy_29way_logOdds',
+               'AC', 'AN', 'AF', 'Freq_group']
+out_format = ['prior', 'pathogenic_mutation', 'control_mutation', 'score', 'freq_group',
+              'select', 'contaminate', 'contaminate_rate', 'auprc', 'pr90', 'pr10']
+
+
+def split_df_gene(df):
+    td = {}
+    value = df.values
+    for line in value:
+        if line[4] not in td:
+            td[line[4]] = [list(line)]
+        else:
+            td[line[4]].append(list(line))
+    return td
+
+
+def get_score_df(patho, ctrl, score, gene_spec=False, random=True, freqg=None, contaminate=None):
+    if freqg:
+        ctrl = ctrl[ctrl['Freq_group'] == freqg]
+        if len(ctrl) == 0:
+            raise MyError('No freq group')
+
+    if gene_spec:
+        total_plist = []
+        total_clist = []
+        patho_df_dict = split_df_gene(patho)
+        ctrl_df_dict = split_df_gene(ctrl)
+        for gene, pvalue in patho_df_dict.items():
+            if gene not in ctrl_df_dict:
+                continue
+            patho_count = len(pvalue)
+            cvalue = ctrl_df_dict[gene]
+            ctrl_count = len(cvalue)
+            if patho_count > ctrl_count:
+                pvalue = sample(pvalue, ctrl_count)
+            else:
+                cvalue = sample(cvalue, len(pvalue))
+            total_plist += pvalue
+            total_clist += cvalue
+
+        patho = DataFrame(total_plist, columns=column_name)
+        ctrl = DataFrame(total_clist, columns=column_name)
+    else:
+        if not contaminate:
+            if random:
+                if len(patho) > len(ctrl):
+                    patho = patho.sample(n=len(ctrl))
+                else:
+                    ctrl = ctrl.sample(n=len(patho))
+        else:
+            try:
+                patho = patho.sample(n=int(contaminate_select + contaminate_select * contaminate))
+            except ValueError:
+                print 'Pathogenic varitants not enough.', 'Contaminate rate is', contaminate, \
+                '\nNeeds', int(contaminate_select + contaminate_select * contaminate), 'Have', len(patho)
+                print 'Not perform contamination.'
+                raise
+
+            pieces = [patho[:contaminate_select], patho[contaminate_select:]]
+            patho = pieces[0]
+
+            try:
+                ctrl = pd.concat([ctrl.sample(n=int(contaminate_select - contaminate_select * contaminate)),
+                                  pieces[1]])
+            except ValueError:
+                print 'ExAC varitants not enough.', 'Contaminate rate is', contaminate, \
+                '\nNeeds', int(contaminate_select - contaminate_select * contaminate), 'Have', len(ctrl)
+                print 'Not perform contamination.'
+                raise
+
+    # Information selection
+    score_df = DataFrame({'score': list(patho[score]) + list(ctrl[score]),
+                          'logic': [True] * len(patho) + [False] * len(ctrl),
+                          'Gene_refGene': list(patho['Gene_refGene']) + list(ctrl['Gene_refGene']),
+                          'AF': list(patho['AF']) + list(ctrl['AF']),
+                          'Freq_group': list(patho['Freq_group']) + list(ctrl['Freq_group'])
+                          })
+    out_file.write('%.3f' % (float(len(patho)) / (len(ctrl) + len(patho))) +
+                     '\t' + '%d' % len(patho) + '\t' + '%d' % len(ctrl) + '\t')
+    if score_df is None:
+        raise
+    return score_df
+
+
+def calc_PRexat(precision, recall, exat):
+    precision = list(precision)
+    recall = list(recall)
+    exat = float(exat)
+    if not (0 <= exat <= 1):
+        raise
+    upexat = 1-exat
+    up = 1.0
+    downexat = -exat
+    down = 0.0
+    for i in recall:
+        if 0 < i-exat < upexat:
+            up = i
+            upexat = i-exat
+        elif downexat < i-exat < 0:
+            down = i
+            downexat = i-exat
+    up_value = upexat/(upexat-downexat)
+    down_value = 1-up_value
+    up_precision = precision[recall.index(up)]
+    down_precision = precision[recall.index(down)]
+    prexat = up_precision * down_value + down_precision * up_value  # the closer the more value, lever principle
+    return prexat
+
+
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
+def neg(value):
+    return 1-value
+
+
+def process_score_df(score_df):
+    precision, recall, thresholds = precision_recall_curve(array(score_df['logic']), array(score_df['score']))
+    auprc = average_precision_score(array(score_df['logic']), array(score_df['score']))
+    if auprc < 0.5:
+        precision = map(neg, precision)
+        auprc = neg(auprc)
+    pr90 = calc_PRexat(precision, recall, 0.90)
+    pr10 = calc_PRexat(precision, recall, 0.10)
+    pgene_count = score_df[score_df['logic'] == True].groupby(['Gene_refGene'])['AF'].count().to_dict()
+    cgene_count = score_df[score_df['logic'] == False].groupby(['Gene_refGene'])['AF'].count().to_dict()
+    pmut_freq = score_df[score_df['logic'] == True]['AF']
+    cmut_freq = score_df[score_df['logic'] == False]['AF']
+    pscore = score_df[score_df['logic'] == True]['score']
+    cscore = score_df[score_df['logic'] == False]['score']
+    return precision, recall, auprc, pr90, pr10, pgene_count, cgene_count, pmut_freq, cmut_freq, pscore, cscore
+
+
+def analysis_data():
+    # Read input
+    patho_df = pd.read_table(import_dir + patho_file)
+    ctrl_df = pd.read_table(import_dir + ctrl_file)
+
+    # Setup output
+    out_file.write('\t'.join(out_format)+'\n')
+    td = {}
+
+    # Calculate
+    for score in score_list:
+        print score
+        for group in group_select_list:
+            print group,
+            try:
+                for select in ['genome', 'gss']:
+                    print select,
+                    if select == 'genome':  # Genome wide select control variants
+                        for contaminate in ['T', 'F']:
+                            print contaminate,
+                            if contaminate == 'F':
+                                # No contamination
+                                index = (score, group, select, 'F', '0')
+                                score_df = get_score_df(patho=patho_df, ctrl=ctrl_df, gene_spec=False,
+                                                        score=score, random=True, freqg=group, contaminate=None)
+                                out_file.write('\t'.join(index))
+                                td[index] = process_score_df(score_df)
+                                out_file.write('\t' + '\t'.join(map(str, td[index][2:5])) + '\n')
+
+                            else:
+                                # contamination
+                                c = contaminate_start
+                                while c <= contaminate_end:
+                                    print c,
+                                    index = (score, group, select, contaminate, '%.2f' % c)
+                                    score_df = get_score_df(patho=patho_df, ctrl=ctrl_df, gene_spec=False,
+                                                            score=score, random=True, freqg=group, contaminate=c)
+                                    out_file.write('\t'.join(index))
+                                    td[index] = process_score_df(score_df)
+                                    out_file.write('\t' + '\t'.join(map(str, td[index][2:5])) + '\n')
+                                    c += contaminate_step
+
+                    else:  # gene specific select control variants
+                        index = (score, group, select, 'F', '0')
+                        score_df = get_score_df(patho=patho_df, ctrl=ctrl_df, gene_spec=True,
+                                                score=score, random=True, freqg=group, contaminate=None)
+                        out_file.write('\t'.join(index))
+                        td[index] = process_score_df(score_df)
+                        out_file.write('\t' + '\t'.join(map(str, td[index][2:5])) + '\n')
+            except MyError:
+                continue
+            print
+        print
+    return td
+
+
+def draw_prc(data_dict, prefix):
+    plt.clf()
+    items = data_dict.items()
+    items.sort()
+    for key, value in items:
+        plt.plot(value[1], value[0], label='{2} AUC={0:0.3f} PR90={1:0.3f}'
+                 .format(value[2], value[3], key))
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(prefix + ' Precision-Recall Curve')
+    plt.legend(loc="lower left")
+    plt.savefig(output_dir + prefix + '.prc.png')
+    return
+
+
+def draw_score_distribution(data_dict, prefix, sel=1, shade=True):
+    plt.clf()
+    items = data_dict.items()
+    items.sort()
+    for key, value in items:
+        plot = sns.distplot(array(value[sel]), hist=False, rug=False, kde_kws={"shade": shade}, label=key)
+        plt.xlabel('Score value')
+        plt.title(prefix + ' Score Distribution')
+        plt.legend(loc="upper left")
+    named = {1: 'control', 0: 'patho'}
+    plot.get_figure().savefig(output_dir + prefix + '_' + named[sel] +'.dist.png')
+    return
+
+
+def draw_gene_count(data_dict, prefix):
+    for key, value in data_dict.items():
+        plt.clf()
+        pd = value[0]
+        cd = value[1]
+        td = {}
+        for gene in pd:
+            if gene in cd:
+                td[gene] = [pd[gene], cd[gene]]
+            else:
+                td[gene] = [pd[gene], 0]
+        for gene in cd:
+            if gene in td:
+                continue
+            else:
+                td[gene] = [0, cd[gene]]
+        x = []
+        y = []
+        for gkey, gvalue in td.items():
+            x.append(gvalue[0])
+            y.append(gvalue[1])
+        sns_plot = sns.jointplot(array(x), array(y), stat_func=None) \
+            .plot_marginals(sns.distplot, kde=False, color=".5")
+        sns_plot.savefig(output_dir + prefix + '_' + key + '.count.png')
+
+
+def draw_plot(td):
+    for score in score_list:
+        #  Draw plot for frequent group prepare
+        freq_pr_dict = {}
+        freq_score_dict = {}
+        freq_gene_dict = {}
+        prefix = '_'.join([score, 'Frequent'])
+        for group in ['I', 'II', 'III', 'IV', 'V', 'VI']:
+            for key, value in td.items():
+                if (score, group, 'genome', 'F') != key[:4]:
+                    continue
+                freq_pr_dict[key[1]] = value[:5]
+                freq_score_dict[key[1]] = value[-2:]
+                freq_gene_dict[key[1]] = value[5:7]
+        draw_prc(freq_pr_dict, prefix)  # freq group PRC
+        draw_score_distribution(freq_score_dict, prefix, sel=1)  # ctrl mut dist against selection
+        draw_score_distribution(freq_score_dict, prefix, sel=0)  # patho mut dist against selection
+        #  draw_gene_count(freq_gene_dict, prefix)  # gene count plot in ctrl and patho
+
+        #  Draw plot for contaminant rate
+        freq_pr_contaminate_dict = {}
+        freq_score_contaminate_dict = {}
+        for key, value in td.items():
+            if score != key[0] or ('genome', 'T') != key[2:4]:
+                continue
+            group = key[1]
+            if group not in freq_score_contaminate_dict:
+                freq_score_contaminate_dict[group] = {key[4]: value[-2:]}
+                freq_pr_contaminate_dict[group] = {key[4]: value[:5]}
+            else:
+                freq_score_contaminate_dict[group][key[4]] = value[-2:]
+                freq_pr_contaminate_dict[group][key[4]] = value[:5]
+
+        for group, gdict in freq_score_contaminate_dict.items():
+            prefix = '_'.join([score, group, 'contamination'])
+            #  draw_score_distribution(gdict, prefix, shade=False)  # ctrl mut dist against
+
+        for group, gdict in freq_pr_contaminate_dict.items():
+            prefix = '_'.join([score, group, 'contamination'])
+            draw_prc(gdict, prefix)  # contaminate rate PRC
+
+    return
+
+
+def analysis_prc():
+    data0 = pd.read_table(output_dir + out_file_name)
+
+    # For contamination analysis
+    data = data0[data0.contaminate == 'T']
+    for score in list(set(list(data.score))):
+        data = data[data.score == score]
+
+        func_dict = {}
+        data_dict = {}
+        group_list = list(set(list(data.freq_group)))
+        group_list.sort()
+        for group in group_list:
+            data_group = data[data.freq_group == group]
+            fit = linear_model.LinearRegression()
+            b = array(data_group.auprc).transpose()
+            a = array([data_group.contaminate_rate]).transpose()
+            y = fit.fit(a, b).predict(a)
+            func_dict[group] = fit
+            data_dict[group] = [a, b, y]
+
+        grey = 0.1
+        plt.clf()
+        for group, data_l in data_dict.items():
+            plt.scatter(data_l[0], data_l[1], color=str(grey))
+            plt.plot(data_l[0], data_l[2], color=str(grey), linewidth=2)
+            grey += 0.15
+        plt.xlim([-0.01, contaminate_end])
+        plt.ylim([min(data.auprc) - 0.003, max(data.auprc) + 0.003])
+        plt.xlabel('Contamination Rate')
+        plt.ylabel('AUPRC')
+        plt.legend(group_list)
+        plt.savefig(output_dir + score + '_contaminate_scatter.png')
+
+        x = 0.0
+        predict_dict = {}
+        predict_dict_0 = {}
+        while x < 1.0:
+            for group, func in func_dict.items():
+                if group not in predict_dict:
+                    predict_dict_0[group] = func.predict(0.0)[0]
+                    predict_dict[group] = [[func.predict(x)[0], x]]
+                else:
+                    predict_dict[group].append([func.predict(x)[0], x])
+            x += 0.001
+
+        result_dict = {}
+        items = predict_dict.items()
+        items.sort()
+        for group, values in items:
+            result_dict[group] = []
+            items0 = predict_dict_0.items()
+            items0.sort()
+            for group_0, value_0 in items0:
+                larger = []
+                larger_x = []
+                smaller = []
+                smaller_x = []
+                for value in values:
+                    if value[0] < value_0:
+                        smaller.append(value[0])
+                        smaller_x.append(value[1])
+
+                    else:
+                        larger.append(value[0])
+                        larger_x.append(value[1])
+                if len(larger) == 0:
+                    result_dict[group].append(0.0)
+                    continue
+                larger_value = min(larger)
+                smaller_value = max(smaller)
+                larger_value_x = larger_x[larger.index(larger_value)]
+                smaller_value_x = smaller_x[smaller.index(smaller_value)]
+                larger_large = larger_value-value_0
+                smaller_small = value_0 - smaller_value
+                if larger_large <= smaller_small:
+                    result_dict[group].append(larger_value_x)
+                else:
+                    result_dict[group].append(smaller_value_x)
+
+        rd = pd.DataFrame(result_dict, index=group_list)
+        plt.clf()
+        mask = np.zeros_like(rd.T)
+        mask[np.triu_indices_from(mask)] = True
+        with sns.axes_style("white"):
+            sns.heatmap(rd.T, linewidths=1, annot=True, mask=mask)
+            plt.title(score+' Relative Contamination Rate')
+            plt.savefig(output_dir + score+'_Relative_Contamination_Rate.count.png')
+
+    return
+
+if __name__ == '__main__':
+    td = analysis_data()
+    draw_plot(td)
+    out_file.close()
+    analysis_prc()
+
